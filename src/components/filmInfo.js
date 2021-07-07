66a56cadc722a3ddbf3aa0ffc5541c78529aa1d0
Siarhei Khalimon <hulk145@gmail.com>
2021-07-07 17:23:12 +0300

    feat: add genres

--- src/components/filmInfo.js
+++ src/components/filmInfo.js
@@ -1,43 +1,62 @@
 // import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
 import noImage from '../images/content/unnamed.jpg';
 import { useParams } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { loadGenres } from '../store/actions';
 import Preloader from './preloader';
 
 const FilmInfo = () => {
   const { id } = useParams();
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(loadGenres());
+  }, [dispatch]);
+
   const { data, isLoaded } = useSelector((state) => state.gallery);
-  // const { genres } = useSelector((state) => state.genres);
+  const { genre } = useSelector((state) => state.genres);
 
   let info = data.find((item) => item.id === +id);
 
-  console.log(info);
   let linkImage = '';
   if (info.poster_path !== null) {
-    linkImage = `https://image.tmdb.org/t/p/w200${info.poster_path}`;
+    linkImage = `https://image.tmdb.org/t/p/w500${info.poster_path}`;
   } else {
     linkImage = noImage;
   }
+  let arrGenres = [];
+
+  for (let j = 0; j < info.genre_ids.length; j++) {
+    const element = info.genre_ids[j];
+    genre.map((item) => {
+      if (element === item.id) {
+        return arrGenres.push(item.name);
+      }
+    });
+  }
+  const genres = arrGenres.join(', ');
+
   const element = (
     <div className="info-page">
       <img className="poster" src={linkImage} alt="poster " />
       <div className="info-film">
-        <h2>title</h2>
-        <p>overview</p>
+        <h2>{info.title}</h2>
+        <p>{info.overview}</p>
         <span>
-          <b>Genres: </b> allGenres
+          <b>Genres: </b> {genres}
         </span>
         <span>
-          <b>Vote average:</b> vote_average
+          <b>Vote average:</b> {info.vote_average}
         </span>
         <span>
-          <b>Vote count:</b> vote_count
+          <b>Vote count:</b> {info.vote_count}
         </span>
         <span>
-          <b>Popularity,:</b> popularity
+          <b>Popularity,:</b> {info.popularity}
         </span>
         <span>
-          <b>Release date:</b> release_date
+          <b>Release date:</b> {info.release_date}
         </span>
       </div>
     </div>
